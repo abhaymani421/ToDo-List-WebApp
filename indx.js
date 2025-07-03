3de2af5c7ba10c7f13f0c969275e427e3b53c4b1
Abhaymani Singh <141544793+abhaymani421@users.noreply.github.com>
2025-07-03 22:07:50 +0530

    Trim input value before adding tasks to prevent leading/trailing spaces

--- indx.js
+++ indx.js
@@ -2,12 +2,13 @@ const inputBox = document.getElementById("input-box") ;
 const listContainer = document.getElementById("list-containers") ;
 
 function addTask(){
-    if( inputBox.value == '' ){
+    const val = inputBox.value.trim();
+    if(val == '' ){
         alert("You must write something!") ;
     }
     else{
         let li = document.createElement("li") ;
-        li.innerHTML = inputBox.value ;
+        li.innerHTML = val ;
         listContainer.appendChild(li) ;
 
         let span = document.createElement("span") ;
